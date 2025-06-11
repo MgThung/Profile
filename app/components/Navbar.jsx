ac2954b93c0558f815abd12b4733c17db8d79b05
MgThung <thunghtike7@gmail.com>
2025-06-11 18:08:33 +0400

    nav

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -29,7 +29,8 @@ const Navbar = ({isDarkMode,setIsDarkMode}) => {
 
   return (
     <>
-    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden className="bg-white dark:bg-black text-black dark:text-white p-4"'>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] 
+    dark:hidden className="bg-white dark:bg-black text-black dark:text-white p-4"'>
       <Image src={assets.header_bg_color} alt='' className='w-full'/>
     </div>
       <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 
@@ -38,7 +39,7 @@ const Navbar = ({isDarkMode,setIsDarkMode}) => {
             <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-14'/>
         </a>
         <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 
-        ${isScroll?"":"bg-white shadow-sm bg-opacity-50"} `}>
+        ${isScroll?"":"bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"} `}>
             <li><a className='font-Ovo' href="#top">Home</a></li>
             <li><a className='font-Ovo' href="#about">About me</a></li>
             <li><a className='font-Ovo' href="#services">Services</a></li>
@@ -46,24 +47,26 @@ const Navbar = ({isDarkMode,setIsDarkMode}) => {
             <li><a className='font-Ovo' href="#contact">Contact me</a></li>
         </ul>
         <div className='flex items-center gap-4'>
-            {/* <button onClick={() => setIsDarkMode(!isDarkMode)}>
+            <button onClick={() => setIsDarkMode(prev=>!prev)}>
               <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt='' className='w-6'/>
-            </button> */}
+            </button>
 
             <a href="#contact" className='hidden lg:flex item-center gap-3 px-10 
             py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo
-            '>Contact <Image src={assets.arrow_icon} alt='' className='w-6'/></a>
+            dark:border-white/50
+            '>Contact <Image src={isDarkMode? assets.arrow_icon_dark : assets.arrow_icon} alt='' className='w-6'/></a>
             <button className='block md:hidden ml-3' onClick={openMenu}>
-              <Image src={assets.menu_black} alt='' className='w-6'/>
+              <Image src={isDarkMode? assets.menu_white : assets.menu_black} alt='' className='w-6'/>
             </button>
         </div>
 
-
         {/* ------------ mobile menu ----------- */}
-        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 
-        z-50 h-screen bg-rose-50 transition duration-500'>
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed 
+        -right-64 top-0 bottom-0 w-64  z-50 h-screen bg-rose-50 transition duration-500
+        dark:bg-darkHover dark:text-white
+        '>
           <div className='absolute right-6 top-6' onClick={closeMenu}>
-            <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+            <Image src={isDarkMode? assets.close_white : assets.close_black} alt='' className='w-5 cursor-pointer'/>
           </div>
             <li><a className='font-Ovo' onClick={closeMenu} href="#top">Home</a></li>
             <li><a className='font-Ovo' onClick={closeMenu} href="#about">About me</a></li>
