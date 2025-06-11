5bdd6a19edc5cd2b88c950f463f377c96a910a95
MgThung <thunghtike7@gmail.com>
2025-06-11 18:41:55 +0400

    dark2

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -53,10 +53,10 @@ const About = ({isDarkMode}) => {
                         whileHover={{scale:1.05}}
                         className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer 
                         hover:bg-lightHover hover:-translate-y-1 duration-500 
-                        hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50'  key={index}>
+                        hover:shadow-black '  key={index}>
                             <Image src={isDarkMode?iconDark: icon} alt={title} className='w-7 mt-3'/>
-                            <h3 className='my-4 font-semibold text-gray-700 dark:text-white'>{title}</h3>
-                            <p className='text-gray-600 text-sm dark:text-white/80'>{description}</p>
+                            <h3 className='my-4 font-semibold text-gray-700 '>{title}</h3>
+                            <p className='text-gray-600 text-sm '>{description}</p>
                         </motion.li>
                     ))}
                 </motion.ul>
@@ -65,7 +65,7 @@ const About = ({isDarkMode}) => {
                     initial={{y:20,opacity:0}}
                     whileInView={{y:0,opacity:1}}
                     transition={{duration:0.5,delay:1.3}}
-                className='my-6 text-gray-700 font-Ovo dark:text-white'>Tools I use</motion.h4>
+                className='my-6 text-gray-700 font-Ovo '>Tools I use</motion.h4>
               <motion.ul
                   initial={{opacity:0}}
                     whileInView={{opacity:1}}
